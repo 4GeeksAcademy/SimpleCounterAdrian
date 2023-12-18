a427b2543290349be524fb7c8b76047014578c64
AdrianRios25 <144917403+AdrianRios2527@users.noreply.github.com>
2023-12-18 20:25:45 +0000

    mejorado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,30 +1,40 @@
 import React from "react";
-import SimplecounterSegundos from "./simplecounterSegundos";
-import SimplecounterMinutos from "./simpleCounterMinutos";
-import SimplecounterHoras from "./simplecounterHoras";
-
-import RelojIcon from "./reloj";
-import Portada from "./titular";
 import Botones from "./botones";
 
-//include images into your bundle
 
-//create your first component
-const Home = () => {
-  return (
-    <div className="bg-black">
-      <Portada ></Portada>
-      <div className="fondo">
-        <RelojIcon></RelojIcon>
-        <SimplecounterHoras></SimplecounterHoras>
-        <SimplecounterMinutos></SimplecounterMinutos>
-        <SimplecounterSegundos></SimplecounterSegundos>
-      </div>
-      <div >
-        <Botones></Botones>
-      </div>
-    </div>
-  );
+const Home = (props) => {
+
+    const segundos = props.counter;
+    let posicion1 = props.counter % 10;
+    let posicion2 = Math.floor((props.counter / 10)%10);
+    let posicion3 =Math.floor((props.counter / 100)%10);
+    let posicion4 = Math.floor((props.counter / 1000)%10);
+    let posicion5 = Math.floor((props.counter / 10000)%10);
+    let posicion6 = Math.floor((props.counter / 100000)%10);
+
+    return (
+        <div className="container">
+            <div className="cajaDelNumero">
+                <span id="segundos" className="numero">{posicion6} </span>
+            </div>
+            <div className="cajaDelNumero">
+                <span id="segundos" className="numero">{posicion5} </span>
+            </div>
+            <div className="cajaDelNumero">
+                <span id="segundos" className="numero">{posicion4} </span>
+            </div>
+            <div className="cajaDelNumero">
+                <span id="segundos" className="numero">{posicion3} </span>
+            </div>
+            <div className="cajaDelNumero">
+                <span id="segundos" className="numero">{posicion2} </span>
+            </div>
+            <div className="cajaDelNumero">
+                <span id="segundos" className="numero">{posicion1} </span>
+            </div>
+           <Botones></Botones>
+        </div>
+    );
 };
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -1,36 +1,23 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import Home from "./component/home";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
-
-window.onload = function () {
-  let segundos = 0;
-  let minutos = 0;
+ let counter = 0
+ //la clave
   
   setInterval(() => {
-      if (segundos <= 60) {
-          setTimeout(() => {
-              document.querySelector("#segundos").innerHTML = `${segundos}`;
-              console.log(segundos);
-              segundos++;
-          }, 1000);
-      } else {
-          minutos++;
-          segundos = 0;
-          document.querySelector("#segundos").innerHTML = `${minutos}`;
-          console.log(minutos);
-      }
-  }, 1000);
+    ReactDOM.render(<Home counter = {counter} />, document.querySelector("#app"));
+    counter++
+    
+  },1000);
+      
 
 
-};
+
 
 
