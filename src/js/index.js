249543ff8ca017316ab8805fd69438611d666653
AdrianRios25 <144917403+AdrianRios2527@users.noreply.github.com>
2023-12-18 17:57:32 +0000

    H

--- src/js/index.js
+++ src/js/index.js
@@ -13,26 +13,7 @@ ReactDOM.render(<Home />, document.querySelector("#app"));
 
 window.onload = function () {
   // Esperar a que la página esté completamente cargada
-  contarHastaDiez();
+
 };
 
-function contarHastaDiez() {
-  var numeroElement = document.querySelector("#numero");
 
-  // Inicializar el contador en 1
-  var contador = 1;
-
-  // Utilizar setInterval para ejecutar la función cada segundo (1000 milisegundos)
-  var intervalo = setInterval(function () {
-    // Mostrar el número actual en el elemento con id "numero"
-    numeroElement.innerHTML = contador;
-
-    // Incrementar el contador
-    contador++;
-
-    // Si hemos alcanzado 10, detener el intervalo
-    if (contador > 10) {
-      clearInterval(intervalo);
-    }
-  }, 1000); // Intervalo de 1000 milisegundos (1 segundo)
-}
