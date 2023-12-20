70ae8176fb2e3e15c23919456211bce020d10ff5
AdrianRios25 <144917403+AdrianRios2527@users.noreply.github.com>
2023-12-20 20:09:31 +0000

    Trabajo realizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,52 +1,65 @@
 import React from "react";
-import Botones from "./botones";
 import Portada from "./portada";
-import RelojIcon from "./reloj";
-
+import { stopInterval ,startInterval ,restartInterval} from "../index.js";
 
 const Home = (props) => {
 
+    
     const segundos = props.counter;
     let posicion1 = props.counter % 10;
-    let posicion2 = Math.floor((props.counter / 10)%10);
-    let posicion3 =Math.floor((props.counter / 100)%10);
-    let posicion4 = Math.floor((props.counter / 1000)%10);
-    let posicion5 = Math.floor((props.counter / 10000)%10);
-    let posicion6 = Math.floor((props.counter / 100000)%10);
+    let posicion2 = Math.floor((props.counter / 10) % 10);
+    let posicion3 = Math.floor((props.counter / 100) % 10);
+    let posicion4 = Math.floor((props.counter / 1000) % 10);
+    let posicion5 = Math.floor((props.counter / 10000) % 10);
+    let posicion6 = Math.floor((props.counter / 100000) % 10);
 
+    
     return (
+        
         <div>
-            <Portada/>
-            
+
+            <Portada />
+
             <div className="container">
-            <RelojIcon/>
-            <div className="cajaDelNumero">
-                <span id="segundos" className="numero">{posicion6} </span>
+
+                <div className="cajaDelNumero">
+                    <span className="numero mt-3">
+                        <i className="fa-regular fa-clock"></i>
+                    </span>
+                </div>
+                <div className="cajaDelNumero">
+                    <span id="segundos" className="numero">{posicion6} </span>
+                </div>
+
+                <div className="cajaDelNumero">
+                    <span id="segundos" className="numero">{posicion5} </span>
+                </div>
+
+                <div className="cajaDelNumero">
+                    <span id="segundos" className="numero">{posicion4} </span>
+                </div>
+
+                <div className="cajaDelNumero">
+                    <span id="segundos" className="numero">{posicion3} </span>
+                </div>
+
+                <div className="cajaDelNumero">
+                    <span id="segundos" className="numero">{posicion2} </span>
+                </div>
+
+                <div className="cajaDelNumero">
+                    <span id="segundos" className="numero">{posicion1} </span>
+                </div>
+
             </div>
 
-            <div className="cajaDelNumero">
-                <span id="segundos" className="numero">{posicion5} </span>
-            </div>
-
-            <div className="cajaDelNumero">
-                <span id="segundos" className="numero">{posicion4} </span>
-            </div>
-
-            <div className="cajaDelNumero">
-                <span id="segundos" className="numero">{posicion3} </span>
-            </div>
-
-            <div className="cajaDelNumero">
-                <span id="segundos" className="numero">{posicion2} </span>
-            </div>
-
-            <div className="cajaDelNumero">
-                <span id="segundos" className="numero">{posicion1} </span>
-            </div>
+            <div className="contenedordebotones">
+                <button onClick={stopInterval} id="stop" className="btn btn-danger">Parar</button>
+                <button onClick={restartInterval} id="restart" className="btn btn-primary">Reiniciar</button>
+                <button onClick={startInterval} id="continue" className="btn btn-success">Seguir</button>
 
             </div>
-                <Botones></Botones>
-           </div>
+        </div>
     );
 };
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,37 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
 import Home from "./component/home";
-
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
+let counter = 0;
+let miIntervalo;
 
- let counter = 0
- //la clave
+function startInterval() {
+  // Iniciar el intervalo
+  miIntervalo = setInterval(() => {
+    ReactDOM.render(<Home counter={counter} />, document.querySelector("#app"));
+    counter++;
+  }, 1000);
+}
+
+function stopInterval() {
+  clearInterval(miIntervalo);
+}
+
+function restartInterval() {
+  // Clear the current interval
+  clearInterval(miIntervalo);
+  // Reset the counter
+  counter = 0;
+  // Restart the interval
+  startInterval();
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Iniciar el intervalo on page load
+  startInterval();
   
-  setInterval(() => {
-    ReactDOM.render(<Home counter = {counter} />, document.querySelector("#app"));
-    counter++
-    
-  },1000);
-      
 
-
-
-
-
+});
+export {stopInterval , startInterval , restartInterval}
